4d4158e4d18921559900d3cf609ca2f7c9535a55
Shai Almog <shai@codenameone.com>
2013-12-11 15:57:05 +0000

    Updated chrome demo lifecycle

--- Demos/Chrome/src/com/codename1/demo/chrome/Chrome.java
+++ Demos/Chrome/src/com/codename1/demo/chrome/Chrome.java
@@ -2,18 +2,24 @@ package com.codename1.demo.chrome;
 
 
 import com.codename1.ui.Display;
+import com.codename1.ui.Form;
 import userclasses.StateMachine;
 
 public class Chrome {
-   
+    private Form current;
     public void init(Object context) {
     }
 
     public void start() {
+        if(current != null) {
+            current.show();
+            return;
+        }
         new StateMachine("/theme");        
     }
 
     public void stop() {
+        current = Display.getInstance().getCurrent();
     }
     
     public void destroy() {
