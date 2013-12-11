e99d54dc20878a868b188e6085802b4c7e4c1ba9
Shai Almog <shai@codenameone.com>
2013-12-11 16:27:21 +0000

    Updated demo lifecycle

--- Demos/Tipster/src/com/codename1/demos/tipster/Tipster.java
+++ Demos/Tipster/src/com/codename1/demos/tipster/Tipster.java
@@ -2,21 +2,27 @@ package com.codename1.demos.tipster;
 
 
 import com.codename1.ui.Display;
+import com.codename1.ui.Form;
 import userclasses.StateMachine;
 
 public class Tipster {
-   
+    private Form currentForm;
     public void init(Object context) {
     }
 
-    public void start(){
+    public void start() {
+        if(currentForm != null) {
+            currentForm.show();
+            return;
+        }
         new StateMachine("/tipster");        
     }
 
-    public void stop(){
+    public void stop() {
+        currentForm = Display.getInstance().getCurrent();
     }
     
-    public void destroy(){    
+    public void destroy() {  
     }
 
 }
