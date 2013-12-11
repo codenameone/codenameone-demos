e517b8afdf38e096c0ea34cf18f005f6096d36b6
Shai Almog <shai@codenameone.com>
2013-12-11 16:11:50 +0000

    Updated demo lifecycle

--- Demos/Mapper/src/com/codename1/demos/mapper/Mapper.java
+++ Demos/Mapper/src/com/codename1/demos/mapper/Mapper.java
@@ -2,18 +2,25 @@ package com.codename1.demos.mapper;
 
 
 import com.codename1.ui.Display;
+import com.codename1.ui.Form;
 import userclasses.StateMachine;
 
 public class Mapper {
-   
+    private Form currentForm;
+    
     public void init(Object context) {
     }
 
     public void start() {
+        if(currentForm != null) {
+            currentForm.show();
+            return;
+        }
         new StateMachine("/theme");        
     }
 
     public void stop() {
+        currentForm = Display.getInstance().getCurrent();
     }
     
     public void destroy() {
