a24f6e73e5f379541e14700e6ef6cfe50e5b61a0
Steve Hannah <steve@weblite.ca>
2015-02-17 22:25:51 +0000

    Updated PieChart demo to demonstrate pinch zoom, panning, and seriesReleased() methods for creating more interactivity.

--- Demos/ChartsDemo/src/com/codename1/demos/charts/BudgetPieChart.java
+++ Demos/ChartsDemo/src/com/codename1/demos/charts/BudgetPieChart.java
@@ -16,12 +16,16 @@
 package com.codename1.demos.charts;
 
 import com.codename1.charts.ChartComponent;
+import com.codename1.charts.models.CategorySeries;
+import com.codename1.charts.models.SeriesSelection;
 import com.codename1.charts.renderers.DefaultRenderer;
 import com.codename1.charts.renderers.SimpleSeriesRenderer;
 import com.codename1.charts.views.PieChart;
 import com.codename1.ui.Form;
 
 import com.codename1.charts.util.ColorUtil;
+import com.codename1.ui.geom.Rectangle;
+import com.codename1.ui.geom.Shape;
 
 
 
@@ -58,20 +62,65 @@ public class BudgetPieChart extends AbstractDemoChart {
   public Form execute() {
     double[] values = new double[] { 12, 14, 11, 10, 19 };
     int[] colors = new int[] { ColorUtil.BLUE, ColorUtil.GREEN, ColorUtil.MAGENTA, ColorUtil.YELLOW, ColorUtil.CYAN };
-    DefaultRenderer renderer = buildCategoryRenderer(colors);
+    final DefaultRenderer renderer = buildCategoryRenderer(colors);
     renderer.setZoomButtonsVisible(true);
     renderer.setZoomEnabled(true);
     renderer.setChartTitleTextSize(20);
     renderer.setDisplayValues(true);
     renderer.setShowLabels(true);
-    SimpleSeriesRenderer r = renderer.getSeriesRendererAt(0);
-    r.setGradientEnabled(true);
-    r.setGradientStart(0, ColorUtil.BLUE);
-    r.setGradientStop(0, ColorUtil.GREEN);
-    r.setHighlighted(true);
-    PieChart chart = new PieChart(buildCategoryDataset("Project budget", values), renderer);
+   
+    final CategorySeries seriesSet = buildCategoryDataset("Project budget", values);
+    final PieChart chart = new PieChart(seriesSet, renderer);
+    ChartComponent comp = new ChartComponent(chart){
+
+        private boolean inDrag = false;
+        
+        @Override
+        public void pointerPressed(int x, int y) {
+            inDrag = false;
+            super.pointerPressed(x, y); //To change body of generated methods, choose Tools | Templates.
+        }
+
+        @Override
+        public void pointerDragged(int x, int y) {
+            inDrag = true;
+            super.pointerDragged(x, y); //To change body of generated methods, choose Tools | Templates.
+        }
+
+        
+        
+        @Override
+        protected void seriesReleased(SeriesSelection sel) {
+            if ( inDrag ){
+                // Don't do this if it was a drag operation
+                return;
+            }
+            for ( SimpleSeriesRenderer r : renderer.getSeriesRenderers()){
+                r.setHighlighted(false);
+            }
+            SimpleSeriesRenderer r = renderer.getSeriesRendererAt(sel.getPointIndex());
+            r.setHighlighted(true);
+            Shape seg = chart.getSegmentShape(sel.getPointIndex());
+            Rectangle bounds = seg.getBounds();
+            bounds = new Rectangle(
+                    bounds.getX()-40,
+                    bounds.getY()-40,
+                    bounds.getWidth()+80,
+                    bounds.getHeight()+80
+            );
+            
+            this.zoomToShapeInChartCoords(bounds, 500);
+            
+            
+        }
+        
+        
+        
+    };
+    comp.setZoomEnabled(true);
+    comp.setPanEnabled(true);
     
-    return wrap("Budget", new ChartComponent(chart));
+    return wrap("Budget", comp);
     
   }
 
